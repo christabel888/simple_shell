18631d87804f9b965f78f88c88abc751a37a3f40
Imran abdulkadir <113688532+Imraneous@users.noreply.github.com>
2022-12-23 22:41:12 +0100

    Update tokenizer.c
    Good

--- tokenizer.c
+++ tokenizer.c
@@ -11,10 +11,10 @@ char **strtow(char *str, char *d)
 {
 	int i, j, k, m, numwords = 0;
 	char **s;
-
 	if (str == NULL || str[0] == 0)
 		return (NULL);
 	if (!d)
+
 		d = " ";
 	for (i = 0; str[i] != '\0'; i++)
 		if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
