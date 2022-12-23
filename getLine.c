cb5a60050e6d2f1b215c0b838937c122de7559f0
Imran abdulkadir <113688532+Imraneous@users.noreply.github.com>
2022-12-23 22:18:13 +0100

    Update getLine.c
    Good

--- getLine.c
+++ getLine.c
@@ -10,6 +10,7 @@
  */
 ssize_t input_buf(info_t *info, char **buf, size_t *len)
 {
+
 	ssize_t r = 0;
 	size_t len_p = 0;
 
@@ -40,6 +41,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 				info->cmd_buf = buf;
 			}
 		}
+
 	}
 	return (r);
 }
